29f94d59a4f2e2cb781c0090bb40d029e737a3da
sara daryaei <sr.daryaei63@gmail.com>
2026-07-03 00:27:41 +0200

    Route EAZO subdomain with middleware

---
+++ middleware.js
@@ -0,0 +1,19 @@
+import { next, rewrite } from "@vercel/functions";
+
+const EAZO_HOST = "eazo.droomit.be";
+
+export default function middleware(request) {
+  const url = new URL(request.url);
+
+  if (url.hostname === EAZO_HOST) {
+    url.pathname = "/client1/index.html";
+    return rewrite(url);
+  }
+
+  return next();
+}
+
+export const config = {
+  matcher: ["/:path*"],
+  runtime: "edge",
+};
